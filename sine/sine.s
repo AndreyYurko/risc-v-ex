e46601201cfac8c7086583c5fa6d6f9affbc34a8
andrey <andrey@gmail.com>
2023-06-27 15:07:07 +0000

    some changes in sine - operate one by one characters

--- sine/sine.s
+++ sine/sine.s
@@ -4,6 +4,7 @@ default_answer = 0x312d
 
 .text
 # if you need some data, put it here
+result: .word 0
 
 .section .text
 
@@ -14,7 +15,19 @@ default_answer = 0x312d
 sine:
 	# implement here
 	
-	li	a3, default_answer
+loop:
+	lbu a0, 0(a1)
+	beqz a0, done
+	li t0, '.'
+	beq t0, a0, next	
+
+	addi a0, a0, -'0'
+	add a3, a3, a0
+	la a3, result
+next:
+	add a1, a1, 1
+	j loop
+done:
 	sw	a3, 0(a2)
 
 
